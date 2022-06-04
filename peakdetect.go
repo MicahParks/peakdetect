b151d87bbb5c6b765c6743b9fb1fb3d8d713d48b
Micah Parks <gpg@micahparks.com>
2022-06-04 13:10:47 -0400

    Add tests based on example

--- peakdetect.go
+++ peakdetect.go
@@ -1,16 +1,25 @@
 package peakdetect
 
 import (
+	"errors"
+	"fmt"
 	"math"
 )
 
 const (
-	SignalNegative = -1
-	SignalNeutral  = 0
-	SignalPositive = 1
+	// SignalNegative indicates that a particular value is a negative peak.
+	SignalNegative Signal = -1
+	// SignalNeutral indicates that a particular value is not a peak.
+	SignalNeutral Signal = 0
+	// SignalPositive indicates that a particular value is a positive peak.
+	SignalPositive Signal = 1
 )
 
+// Signal is a set of enums that indicates what type of peak, if any a particular value is.
 type Signal int8
+
+// ErrInvalidInitialValues indicates that the initial values provided are not valid to initialize a PeakDetector.
+var ErrInvalidInitialValues = errors.New("the initial values provided are invalid")
 
 type peakDetector struct {
 	index       uint
@@ -22,30 +31,73 @@ type peakDetector struct {
 	threshold   float64
 }
 
+// PeakDetector detects peaks in realtime timeseries data using z-scores.
+//
+// This is a Golang interface for the algorithm described by this StackOverflow answer:
+// https://stackoverflow.com/a/22640362/14797322
+//
+// Brakel, J.P.G. van (2014). "Robust peak detection algorithm using z-scores". Stack Overflow. Available
+// at: https://stackoverflow.com/questions/22583391/peak-signal-detection-in-realtime-timeseries-data/22640362#22640362
+// (version: 2020-11-08).
 type PeakDetector interface {
-	Initialize(config Config, values []float64) error
+	// Initialize initializes the PeakDetector with its configuration and initialValues. The initialValues are the first
+	// values to be processed by the PeakDetector. The length of these values are used to configure the PeakDetector's
+	// lag (see description below). The PeakDetector will never return any signals for the initialValues.
+	//
+	// influence determines the influence of signals on the algorithm's detection threshold. If put at 0, signals have
+	// no influence on the threshold, such that future signals are detected based on a threshold that is calculated with
+	// a mean and standard deviation that is not influenced by past signals. If put at 0.5, signals have half the
+	// influence of normal data points. Another way to think about this is that if you put the influence at 0, you
+	// implicitly assume stationary (i.e. no matter how many signals there are, you always expect the time series to
+	// return to the same average over the long term). If this is not the case, you should put the influence parameter
+	// somewhere between 0 and 1, depending on the extent to which signals can systematically influence the time-varying
+	// trend of the data. E.g., if signals lead to a structural break of the long-term average of the time series, the
+	// influence parameter should be put high (close to 1) so the threshold can react to structural breaks quickly.
+	//
+	// threshold is the number of standard deviations from the moving mean above which the algorithm will classify a new
+	// datapoint as being a signal. For example, if a new datapoint is 4.0 standard deviations above the moving mean and
+	// the threshold parameter is set as 3.5, the algorithm will identify the datapoint as a signal. This parameter
+	// should be set based on how many signals you expect. For example, if your data is normally distributed, a
+	// threshold (or: z-score) of 3.5 corresponds to a signaling probability of 0.00047 (from this table), which implies
+	// that you expect a signal once every 2128 datapoints (1/0.00047). The threshold therefore directly influences how
+	// sensitive the algorithm is and thereby also determines how often the algorithm signals. Examine your own data and
+	// choose a sensible threshold that makes the algorithm signal when you want it to (some trial-and-error might be
+	// needed here to get to a good threshold for your purpose).
+	//
+	// lag determines how much your data will be smoothed and how adaptive the algorithm is to changes in the long-term
+	// average of the data. The more stationary your data is, the more lags you should include (this should improve the
+	// robustness of the algorithm). If your data contains time-varying trends, you should consider how quickly you want
+	// the algorithm to adapt to these trends. I.e., if you put lag at 10, it takes 10 'periods' before the algorithm's
+	// threshold is adjusted to any systematic changes in the long-term average. So choose the lag parameter based on
+	// the trending behavior of your data and how adaptive you want the algorithm to be.
+	Initialize(influence, threshold float64, initialValues []float64) error
+	// Next processes the next value and determines its signal.
 	Next(value float64) Signal
+	// NextBatch processes the next values and determines their signals. Their signals will be returned in a slice equal
+	// to the length of the input.
 	NextBatch(values []float64) []Signal
 }
 
-func (p *peakDetector) Initialize(config Config, initialValues []float64) error {
-	length := len(initialValues) // TODO This is the lag?
-	if length == 0 {
-		// TODO Return error.
+// NewPeakDetector creates a new PeakDetector. It must be initialized before use.
+func NewPeakDetector() PeakDetector {
+	return &peakDetector{}
+}
+
+func (p *peakDetector) Initialize(influence, threshold float64, initialValues []float64) error {
+	p.lag = uint(len(initialValues))
+	if p.lag == 0 {
+		return fmt.Errorf("the length of the intial values is zero, the length is used as the lag for the algorithm: %w", ErrInvalidInitialValues)
 	}
-	p.lag = uint(length)
-	p.influence = config.Influence
-	p.threshold = config.Threshold
-	// TODO Validate config.
-	// TODO Remove configuration.
-	mean, stdDev := meanStdDev(initialValues)
-	p.cache = make([]float64, length)
+	p.influence = influence
+	p.threshold = threshold
+
+	p.cache = make([]float64, p.lag)
 	copy(p.cache, initialValues)
-	p.meanCache = make([]float64, length)
-	p.stdDevCache = make([]float64, length)
-	p.meanCache[length-1] = mean
-	p.stdDevCache[length-1] = stdDev
-	p.index = uint(length) - 1
+	p.meanCache = make([]float64, p.lag)
+	p.stdDevCache = make([]float64, p.lag)
+
+	p.meanCache[p.index], p.stdDevCache[p.index] = meanStdDev(initialValues)
+
 	return nil
 }
 
@@ -76,17 +128,25 @@ func (p *peakDetector) Next(value float64) (signal Signal) {
 }
 
 func (p *peakDetector) NextBatch(values []float64) []Signal {
-	signals := make([]Signal, len(values))
+	signals := make([]Signal, len(values)) // TODO Test this method.
 	for i, v := range values {
 		signals[i] = p.Next(v)
 	}
 	return signals
 }
 
-func NewPeakDetector() PeakDetector {
-	return &peakDetector{}
-}
+// meanStdDev determines the mean and population standard deviation for the given population.
+func meanStdDev(sample []float64) (mean, stdDev float64) {
+	for _, num := range sample {
+		mean += num
+	}
+	mean /= float64(len(sample))
 
-func main() {
-	println("compiled & ran")
+	for _, num := range sample {
+		stdDev += math.Pow(num-mean, 2)
+	}
+	stdDev /= float64(len(sample))
+	stdDev = math.Sqrt(stdDev)
+
+	return mean, stdDev
 }
---
+++ peakdetect_test.go
@@ -0,0 +1,62 @@
+package peakdetect_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/MicahParks/peakdetect"
+)
+
+const (
+	exampleLag       = 30
+	exampleInfluence = 0
+	exampleThreshold = 5
+
+	logFmt = "%s\nError: %s"
+)
+
+var (
+	exampleInputs  = []float64{1, 1, 1.1, 1, 0.9, 1, 1, 1.1, 1, 0.9, 1, 1.1, 1, 1, 0.9, 1, 1, 1.1, 1, 1, 1, 1, 1.1, 0.9, 1, 1.1, 1, 1, 0.9, 1, 1.1, 1, 1, 1.1, 1, 0.8, 0.9, 1, 1.2, 0.9, 1, 1, 1.1, 1.2, 1, 1.5, 1, 3, 2, 5, 3, 2, 1, 1, 1, 0.9, 1, 1, 3, 2.6, 4, 3, 3.2, 2, 1, 1, 0.8, 4, 4, 2, 2.5, 1, 1, 1}
+	exampleOutputs = []peakdetect.Signal{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0}
+)
+
+func TestPeakDetector_Initialize(t *testing.T) {
+	detector := peakdetect.NewPeakDetector()
+	err := detector.Initialize(0, 0, nil)
+	if !errors.Is(err, peakdetect.ErrInvalidInitialValues) {
+		t.Fatalf("Invalid initilization did not produce error.\n  Expected: %s\n  Actual: %s", peakdetect.ErrInvalidInitialValues, err)
+	}
+}
+
+func TestPeakDetector_Next(t *testing.T) {
+	detector := peakdetect.NewPeakDetector()
+	err := detector.Initialize(exampleInfluence, exampleThreshold, exampleInputs[0:exampleLag])
+	if err != nil {
+		t.Fatalf(logFmt, "Error during initilization.", err)
+	}
+
+	for i, v := range exampleInputs[exampleLag:] {
+		signal := detector.Next(v)
+		exampleSignal := exampleOutputs[i+exampleLag]
+		match := exampleSignal == signal
+		if !match {
+			t.Fatalf("Example signal did not match actual signal.\n  Example: %d\n  Actual: %d", exampleSignal, signal)
+		}
+	}
+}
+
+func TestPeakDetector_NextBatch(t *testing.T) {
+	detector := peakdetect.NewPeakDetector()
+	err := detector.Initialize(exampleInfluence, exampleThreshold, exampleInputs[0:exampleLag])
+	if err != nil {
+		t.Fatalf(logFmt, "Error during initilization.", err)
+	}
+
+	signals := detector.NextBatch(exampleInputs[exampleLag:])
+	for i, signal := range signals {
+		exampleSignal := exampleOutputs[i+exampleLag]
+		if signal != exampleSignal {
+			t.Fatalf("Example signal did not match actual signal.\n  Example: %d\n  Actual: %d", exampleSignal, signal)
+		}
+	}
+}
